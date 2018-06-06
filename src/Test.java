22d19592025b224877647be96e914513b586702b
TheIncgi <theincgi@gmail.com>
2018-06-06 03:48:01 -0400

    Removed un-needed test code

--- src/Test.java
+++ src/Test.java
@@ -9,26 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		Globals g = JsePlatform.debugGlobals();
 		LuaFunction require = g.get("require").checkfunction();
-		System.out.println(require);
+		
 		LuaValue v = require.call("HelloWorld");
 		System.out.println(v);
 		v.get("test").call();
-		
-	//	System.out.println(LuaValue.valueOf((String)null));
-		
-		//invoke vs arg#call
-		LuaFunction f = new OneArgFunction() {
-			@Override
-			public LuaValue call(LuaValue arg) {
-				System.out.println(arg.tojstring());
-				return NONE;
-			}
-		};
-		LuaTable t = new LuaTable();
-		t.set(1, "U");
-		t.set(2, "V");
-		f.invoke(t.unpack());
-		
 	}
 	
 	
